f8efbf44fc4c93c06268db413aeb99b19962d33a
Tausif Zaman <tausifzmn4@gmail.com>
2017-11-16 09:55:43 -0600

    Update hack.s

--- hack.s
+++ hack.s
@@ -4,12 +4,12 @@
 .func main
 
 main:
-  BL inp_rtvr
+  BL inp_rtv
   BL _divide
   BL prnt_prcd
   B main
-  
- _scanf:
+
+_scanf:
   PUSH {LR}
   SUB SP, SP, #4
   LDR R0, =format_str
@@ -18,8 +18,8 @@ main:
   LDR R0, [SP]
   ADD SP, SP, #4
   POP {PC}
-  
-inp_rtvr:
+
+inp_rtv:
   PUSH {LR}
   MOV R7, #4
   MOV R0, #1
@@ -28,7 +28,7 @@ inp_rtvr:
   SWI 0
   BL _scanf
   PUSH {R0}
-  
+
   MOV R7, #4
   MOV R0, #1
   MOV R2, #3
@@ -38,46 +38,45 @@ inp_rtvr:
   POP {R9}
   MOV R10, R0
   POP {PC}
-  
+
 _divide:
   MOV R4, LR
   VMOV S0, R9
   VMOV S1, R10
-  VCVT.F32.U32 S0, S0
-  VCVT.F32 S1, S1
-  
+  VCVT.F32.S32 S0, S0
+  VCVT.F32.S32 S1, S1
+
   VDIV.F32 S2, S0, S1
-  
-  VCVT.F64.U32 D4, S2
+
+  VCVT.F64.F32 D4, S2
   VMOV R1, R2, D4
   PUSH {R1}
   PUSH {R2}
   PUSH {R9}
   PUSH {R10}
   MOV PC, R4
-  
+
 prnt_prcd:
   MOV R4, LR
   POP {R2}
   POP {R1}
   LDR R0, =prnt_stm1
   BL printf
-  
+
   POP {R2}
   POP {R1}
   BL prnt_float
   MOV PC, R4
-  
+
 prnt_float:
   PUSH {LR}
   LDR R0, =prnt_stm2
   BL printf
   POP {PC}
-  
+
 .data
 n_prmt:        .ascii "n: "
 d_prmt:        .ascii "d: "
 format_str:    .asciz "%d"
 prnt_stm1:     .asciz "%d / %d ="
-prnt_stm2:     .asciz "%f\n\n" 
-  
+prnt_stm2:     .asciz "%f\n\n"
